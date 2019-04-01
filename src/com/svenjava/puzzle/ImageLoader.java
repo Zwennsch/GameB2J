03ecbf9f49570073eb06937aff57a4a6be80eabd
Sven Schroeder <schuen@gmx>
2019-04-01 17:13:21 +0200

    updated ImageLoader class und starting on image creating in PuzzleEx main class

--- src/com/svenjava/puzzle/ImageLoader.java
+++ src/com/svenjava/puzzle/ImageLoader.java
@@ -1,5 +1,6 @@
 package com.svenjava.puzzle;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,13 +9,13 @@ import javax.imageio.ImageIO;
 
 public class ImageLoader {
 	BufferedImage src;
-	static BufferedImage resizedImage;
+	private BufferedImage resizedImage;
 	
 	public ImageLoader(String source, int desiredWidth) {
 		try {
 			src = loadImage(source);
 			int h = getNewHeight(src.getWidth(), src.getHeight(), desiredWidth);
-			resizedImage = resizeImage(src, desiredWidth, h, BufferedImage.TYPE_INT_ARGB);
+			setResizedImage(resizeImage(src, desiredWidth, h, BufferedImage.TYPE_INT_ARGB));
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -22,9 +23,11 @@ public class ImageLoader {
 		
 	}
 
-	private BufferedImage resizeImage(BufferedImage src2, int desiredWidth, int h, int typeIntArgb) {
-		
-		return null;
+	private BufferedImage resizeImage(BufferedImage src2, int width, int height, int typeIntArgb) {
+		BufferedImage bIm = new BufferedImage(width, height, typeIntArgb);
+		Graphics2D g = bIm.createGraphics();
+		g.drawImage(src2, 0, 0, null);
+		return bIm;
 	}
 
 	private int getNewHeight(int width, int height, int desiredWidth) {
@@ -39,6 +42,14 @@ public class ImageLoader {
 		img = ImageIO.read(new File(source));
 		
 		return img;
+	}
+
+	public BufferedImage getResizedImage() {
+		return resizedImage;
+	}
+
+	public void setResizedImage(BufferedImage resizedImage) {
+		this.resizedImage = resizedImage;
 	}
 
 }
--- src/com/svenjava/puzzle/PuzzleEx.java
+++ src/com/svenjava/puzzle/PuzzleEx.java
@@ -1,13 +1,17 @@
 package com.svenjava.puzzle;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.LayoutManager;
 import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.awt.image.FilteredImageSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +22,17 @@ import javax.swing.JPanel;
 public class PuzzleEx extends JFrame{
 	
 	private JPanel panel;
+	private static final String  SOURCE = "src/resources/claudio-pizarro.jpg";
+	private int width, height;
+	
+	private BufferedImage resizedImage;
+	private Image image;
 	
 	List<Point> solution;
 	List<MyButton> buttons;
+	
+	public final int NUMBER_OF_ROWS = 4;
+	public final int NUMBER_OF_COLUMNS = 3;
 	
 	public final int NUMBER_OF_BUTTONS = 12;
 	private final int DESIRED_WIDTH= 300;
@@ -51,6 +63,19 @@ public class PuzzleEx extends JFrame{
 		panel.setBorder(BorderFactory.createLineBorder(Color.gray));
 		panel.setLayout(new GridLayout(4, 3, 0, 0));
 		
+		ImageLoader iLoader = new ImageLoader(SOURCE, DESIRED_WIDTH);
+		resizedImage = iLoader.getResizedImage();
+		width = resizedImage.getWidth();
+		height = resizedImage.getHeight();
+		
+		add(panel, BorderLayout.CENTER);
+		
+		for (int i = 0; i < NUMBER_OF_ROWS; i++) {
+			for (int j = 0; j < NUMBER_OF_COLUMNS; j++) {
+				image = createImage(new FilteredImageSource(resizedImage.getSource(), imgf))
+				
+			}
+		}
 	}
 
 	public static void main(String[] args) {
